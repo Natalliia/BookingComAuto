197fc6c2f2aa959d91fc89dc77a18201d4793e53
Nataliia <shev.natash@gmail.com>
2019-05-29 18:39:39 +0300

    test

--- BookingComAuto/Program.cs
+++ BookingComAuto/Program.cs
@@ -11,6 +11,31 @@ namespace BookingComAuto
         static void Main(string[] args)
         {
             Console.WriteLine("Hi");
+            Console.WriteLine("Hi again");
+
+
+            List<Company> people = new List<Company>(3);
+            people.Add(new Company() { Name = "Microsoft" });
+            people.Add(new Company() { Name = "Microsoft" });
+            people.Add(new Company() { Name = "Microsoft" });
+            people.Add(new Company() { Name = "Google" });
+            people.Add(new Company() { Name = "Microsoft" });
+            people.Add(new Company() { Name = "Microsoft" });
+            people.Add(new Company() { Name = "Google" });
+            people.Add(new Company() { Name = "Apple" });
+            people.Add(new Company() { Name = "Google" });
+
+            List<string> myList3 = new List<string>();
+            foreach (Company p in people)
+            {
+                myList3.Add(p.Name);
+                Console.WriteLine(p.Name);
+            }
+            Console.WriteLine("*********************");
+            foreach (string t in myList3)
+            {
+                Console.WriteLine(t);
+            }
         }
     }
 }
